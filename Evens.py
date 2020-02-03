96f59ab22cb03f9501c96f4d6cb953c225bdbfe0
as1266 <43043130+as1266@users.noreply.github.com>
2020-02-03 00:16:01 -0600

    Delete Evens.py

--- Evens.py
+++
@@ -1,9 +0,0 @@
-# 8: Write a program that counts the number of even digits in the scope of 1—1000.
-evenCount = 0
-for number in range(1, 1001):
-    if (number % 2) == 0:
-        print("The number ", number, "is even!")
-        evenCount = evenCount+1
-print("The count of even numbers is ", evenCount, "!")
-
-
