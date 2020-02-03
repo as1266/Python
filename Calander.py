4e656b9e9e9aa3ae095f16b6179f20475270558f
as1266 <43043130+as1266@users.noreply.github.com>
2020-02-03 00:15:19 -0600

    Delete Calander.py

--- Calander.py
+++
@@ -1,6 +0,0 @@
-# 4:Write a program that uses a for loop to print( January to December)
-# One of the months of the year is January
-# One of the months of the year is February ...
-calander = 'January', "February", "March ", "April", "May", "June", "July", "August", "September", "October", "November", "December "
-for i in calander:
-        print("One of the month of the year is ", i, )
